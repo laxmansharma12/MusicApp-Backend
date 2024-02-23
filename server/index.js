a6658bdb04f63384afa2ca86c0abf04bf2a9b034
laxman <laxmansharmawe@gmail.com>
2024-02-23 11:29:45 +0530

    fixed CORS for server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import playlistRoutes from "./Routes/playlistRoutes.js";
 import musicRoutes from "./Routes/musicRoutes.js";
 import cors from "cors";
 
-//confugure env
+//configure env
 dotenv.config();
 
 //database config
@@ -17,12 +17,11 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(cors());
+app.use(cors()); // Use cors middleware to handle CORS
 app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/music", musicRoutes);
 app.use("/api/v1/playlist", playlistRoutes);
@@ -30,14 +29,6 @@ app.use("/api/v1/playlist", playlistRoutes);
 //rest api
 app.get("/", (req, res) => {
 	res.send("Welcome to MyMusic App");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"PUT, POST, GET, DELETE, PATCH, OPTIONS"
-	);
 });
 
 //PORT
