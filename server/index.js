c0c366efc481b08508845122a1484da0abbd3d24
laxman <laxmansharmawe@gmail.com>
2024-02-23 11:13:17 +0530

    fixed CORS for server

--- server/index.js
+++ server/index.js
@@ -17,12 +17,7 @@ connectDB();
 const app = express();
 
 //middlewares
-const corsOptions = {
-	origin: "http://localhost:3000",
-	credentials: true, //access-control-allow-credentials:true
-	optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -35,6 +30,14 @@ app.use("/api/v1/playlist", playlistRoutes);
 //rest api
 app.get("/", (req, res) => {
 	res.send("Welcome to MyMusic App");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"PUT, POST, GET, DELETE, PATCH, OPTIONS"
+	);
 });
 
 //PORT
