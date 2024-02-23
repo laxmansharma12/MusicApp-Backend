52a6a59c005251526d69ad2cca52a3101fd33c9f
laxman <laxmansharmawe@gmail.com>
2024-02-23 10:46:17 +0530

    fixed CORS for server

--- server/index.js
+++ server/index.js
@@ -17,7 +17,12 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(cors());
+const corsOptions = {
+	origin: "https://music-app-backend-chi.vercel.app",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 
